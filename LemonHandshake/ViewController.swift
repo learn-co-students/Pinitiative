60c2ba7a1194fcede00cc88defc31ab993658b7c
Christopher Boynton <flatironschool@Flatirons-MacBook-Air-67.local>
2016-11-14 17:27:47 -0500

    Quick test of FirebaseAuth

--- LemonHandshake/ViewController.swift
+++ LemonHandshake/ViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-        
-
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let error = FirebaseAuth.signUpUserWith(email: "cboynton16@gmail.com", password: "password")
+        if let error = error {
+            print(error.localizedDescription)
+        }
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
 
