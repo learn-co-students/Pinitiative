7d0796e4b5ec50fb82585d9cca460c8f70ee4a4d
tameika <tameika.lawrence@gmail.com>
2016-11-15 13:33:45 -0500

    2 more storyboards added: 8 total

---
+++ LemonHandshake/MemberManagementViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MemberManagementViewController.swift
+//  LemonHandshake
+//
+//  Created by Tameika Lawrence on 11/15/16.
+//  Copyright © 2016 Flatiron School. All rights reserved.
+//
+
+import UIKit
+
+class MemberManagementViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
