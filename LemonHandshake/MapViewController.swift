1b368b9153be0ab3382506959fa1345e4e7dba7c
Jhantelleb <jhantelle.belleza@gmail.com>
2016-11-18 09:16:20 -0500

    Added dummy points

--- LemonHandshake/MapViewController.swift
+++ LemonHandshake/MapViewController.swift
@@ -21,6 +21,8 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
         createMap()
         view.addSubview(mapView)
         mapView.delegate = self
+        store.generateData()
+        addPointAnnotations()
 //        activateGestureRecognizer()
     }
 
@@ -46,21 +48,63 @@ class MapViewController: UIViewController, MGLMapViewDelegate {
         mapView.styleURL = store.styleURL
         mapView.showsUserLocation = true
         mapView.userTrackingMode = .follow
-        mapView.zoomLevel = 15
+        mapView.zoomLevel = 5
         mapView.frame.size.height = view.frame.size.height * 0.93
     }
     
     func mapView(_ mapView: MGLMapView, didUpdate userLocation: MGLUserLocation?) {
         guard let userLocation = mapView.userLocation else { return }
         let center = CLLocationCoordinate2DMake(userLocation.coordinate.latitude, userLocation.coordinate.longitude)
-        store.latitude = userLocation.coordinate.latitude
-        store.longitude = userLocation.coordinate.longitude
-        mapView.latitude = store.latitude
-        mapView.longitude = store.longitude
+        store.userCoordinate = userLocation.coordinate
+        mapView.latitude = 40.771336
+        mapView.longitude = -73.919845
         mapView.setCenter(center, animated: true)
     }
     
     
+    func addPointAnnotations() {
+        var pointAnnotations = [MGLPointAnnotation]()
+        
+        let locations = store.locations
+        
+        for location in locations {
+            let point = MGLPointAnnotation() //do not take this out of the for loop! It needs to instantiate a new point everytime it loops.
+            point.coordinate = location.coordinates
+            point.title = location.name
+            point.subtitle = location.address
+            pointAnnotations.append(point)
+        }
+        mapView.addAnnotations(pointAnnotations)
+    }
+
+    func mapView(_ mapView: MGLMapView, viewFor annotation: MGLAnnotation) -> MGLAnnotationView? {
+//        // This example is only concerned with point annotations.
+//        guard annotation is MGLPointAnnotation else {
+//            return nil
+//        }
+//
+//        // Use the point annotation’s longitude value (as a string) as the reuse identifier for its view.
+//        let reuseIdentifier = "\(annotation.coordinate.longitude)"
+//        
+//        // For better performance, always try to reuse existing annotations.
+//        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: reuseIdentifier)
+//        
+//        // If there’s no reusable annotation view available, initialize a new one.
+//        if annotationView == nil {
+//            annotationView = CustomAnnotationView(reuseIdentifier: reuseIdentifier)
+//            annotationView!.frame = CGRect(x: 0, y: 0, width: 40, height: 40)
+//            
+//            // Set the annotation view’s background color to a value determined by its longitude.
+//            let hue = CGFloat(annotation.coordinate.longitude) / 100
+//            annotationView!.backgroundColor = UIColor(hue: hue, saturation: 0.5, brightness: 1, alpha: 1)
+//        }
+        
+//        return annotationView
+        return nil
+    }
     
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
 }
 
