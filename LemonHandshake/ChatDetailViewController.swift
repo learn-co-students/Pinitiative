42447c7133716ff7f5285ff6613f3f703f9c4903
tameika <tameika.lawrence@gmail.com>
2016-11-20 15:13:35 -0500

    Building chat VC

--- LemonHandshake/ChatDetailViewController.swift
+++ LemonHandshake/ChatDetailViewController.swift
@@ -19,15 +19,25 @@ class ChatDetailViewController: JSQMessagesViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.chatCollectionView.reloadData()
     }
 
     let layout: JSQMessagesCollectionViewFlowLayout = JSQMessagesCollectionViewFlowLayout()
+    let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
     
-    let chatCollectionView: JSQMessagesCollectionView = JSQMessagesCollectionView(frame: self.view.frame, collectionViewLayout: layout)
+    //let chatCollectionView: JSQMessagesCollectionView = JSQMessagesCollectionView
     
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.messages.count
+    }
     
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, messageDataForItemAt indexPath: IndexPath!) -> JSQMessageData! {
+        
+        let data = self.messages[indexPath.row]
+        return data
+    }
+
     
  
 }
