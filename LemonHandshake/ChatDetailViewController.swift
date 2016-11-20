659522bb70a0cc1d2ab71c6f696b9964aa3a7433
tameika <tameika.lawrence@gmail.com>
2016-11-20 18:50:13 -0500

    Building chat VC

--- LemonHandshake/ChatDetailViewController.swift
+++ LemonHandshake/ChatDetailViewController.swift
@@ -11,10 +11,14 @@ import JSQMessagesViewController
 
 class ChatDetailViewController: JSQMessagesViewController {
     
+    var chatCollectionView: JSQMessagesCollectionView!
+    
     let incomingBubble = JSQMessagesBubbleImageFactory().incomingMessagesBubbleImage(with: UIColor.orange)
-    let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: UIColor.lightGray)
+    let outgoingBubble = JSQMessagesBubbleImageFactory().outgoingMessagesBubbleImage(with: UIColor.blue)
     var messages = [JSQMessage]()
     
+    var sendersId: String!
+    var sendersDisplayName: String!
     
 
     override func viewDidLoad() {
@@ -23,10 +27,20 @@ class ChatDetailViewController: JSQMessagesViewController {
         self.chatCollectionView.reloadData()
     }
 
+    // FOR FORMING VIEW CONTROLLER
+    
+ 
+    
+    
     let layout: JSQMessagesCollectionViewFlowLayout = JSQMessagesCollectionViewFlowLayout()
     let frame = CGRect(x: 0, y: 0, width: 100, height: 100)
     
     //let chatCollectionView: JSQMessagesCollectionView = JSQMessagesCollectionView
+    
+    
+    
+    
+    //FOR HANDLING MESSAGES
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return self.messages.count
@@ -37,10 +51,50 @@ class ChatDetailViewController: JSQMessagesViewController {
         let data = self.messages[indexPath.row]
         return data
     }
-
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, didDeleteMessageAt indexPath: IndexPath!) {
+        self.messages.remove(at: indexPath.row)
+    }
+    
+    
+    
+    
+    // FOR MESSAGE BUBBLES ATTRIBUTES
+    
+    override func collectionView(_ collectionView: JSQMessagesCollectionView!, avatarImageDataForItemAt indexPath: IndexPath!) -> JSQMessageAvatarImageDataSource! {
+        return nil
+    }
+    
+/*    func collectionView(_ collectionView: JSQMessagesCollectionView!, messageBubbleImageDataForItemAt indexPath: IndexPath!) -> JSQMessageBubbleImageDataSource! {
+    
+  } */
     
  
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //extension ChatDetailViewController {
