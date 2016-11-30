6db39424ea683d6c97a061640acfa5282c32a9f9
tameika <tameika.lawrence@gmail.com>
2016-11-30 10:57:29 -0500

    Cleaning and connecting chat

--- LemonHandshake/InitiativeDetailViewController.swift
+++ LemonHandshake/InitiativeDetailViewController.swift
@@ -131,14 +131,19 @@ class InitiativeDetailViewController: UIViewController {
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        
+        if segue.identifier == "chatButtonSegue" {
+            let dest = segue.destination as? ChatDetailViewController
+            dest?.initiative =
+        }
     }
-    */
+    
 
 }
